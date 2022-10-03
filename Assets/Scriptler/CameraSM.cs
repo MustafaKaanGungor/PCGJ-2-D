68b21c42b42ce0c4ea9099356947bf82dc192c0f
BringSalavat <kaan062004+github@gmail.com>
2022-10-03 22:10:39 +0300

    added movement(tried)

--- Assets/Scriptler/CameraSM.cs
+++ Assets/Scriptler/CameraSM.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraSM : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    public float smoothing;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(transform.position != target.position)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        }
     }
 }
--- Assets/Scriptler/Player.cs
+++ Assets/Scriptler/Player.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float accelerationForce;
+    public float maxSpeed;
+    public float timeZeroToMax;
+    public Rigidbody2D GregRB;
+    private float moveInput;
+    private Vector3 scale;
     void Start()
     {
-        
+        scale = this.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        
+        moveInput = Input.GetAxisRaw("Horizontal");
+        GregRB.AddForce(Vector2.right * moveInput * accelerationForce);
+        Vector2 clampedVelocity = GregRB.velocity;
+        clampedVelocity.x = Mathf.Clamp(GregRB.velocity.x, -maxSpeed, maxSpeed);
+        GregRB.velocity = clampedVelocity;
+
+        if (moveInput > 0)
+        {
+            scale.x = 1;
+            scale.y = 1;
+            this.transform.localScale = scale;
+        }
+        else if(moveInput < 0)
+        {
+            scale.x = -1;
+            scale.y = 1;
+            this.transform.localScale = scale;
+        }
     }
 }
